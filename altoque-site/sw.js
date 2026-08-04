b5099220d4f0ac2b96d75d06a884bf6843f1f04f
martinhida-glitch <martinhida@gmail.com>
2026-08-04 05:14:09 -0300

    Actualizar recursos offline de ALTOQUE

--- altoque-site/sw.js
+++ altoque-site/sw.js
@@ -1,5 +1,5 @@
-const CACHE='altoque-v2-20260804b';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./styles.css','./app.js','./app-core.js','./app-features.js','./app-init.js'];
+const CACHE='altoque-v2-20260804c';
+const CORE=['./','./index.html','./manifest.json','./icon.svg','./styles.css','./app.js','./app-core.js','./app-features.js','./app-init.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
